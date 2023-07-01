308ad2e702aca37dacba256158515af1db474903
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-07-01 13:31:36 -0300

    componente da lista de tasks criada

---
+++ src/components/List View/ListView.jsx
@@ -0,0 +1,28 @@
+import styles from './ListView.module.scss'
+
+const ListView = () => {
+    const tasks = [
+        "Task 01",
+        "Task 02",
+        "Task 03",
+        "Task 04",
+        "Task 05",
+    ]
+
+    return (
+        <div className={styles.container}>
+            <div className="list">
+                <ul>
+                    {
+                        tasks.map(task => (
+                            <li key={task}>{task}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+
+        </div>
+    );
+};
+
+export default ListView;
