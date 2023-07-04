48f37644879be108c44586f5418a779512320829
Gabriel Ferreira <willamigabrielf.souza@gmail.com>
2023-07-04 11:00:36 -0300

    update

--- src/routes/TaskDetails/TaskDetails.jsx
+++ src/routes/TaskDetails/TaskDetails.jsx
@@ -1,14 +1,38 @@
 import styles from './TaskDetails.module.scss'
 import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+
 
 
 const TaskDetails = () => {
     const {id} = useParams();
+    const [pickedTask, setPickedTask] = useState({});
+    const [editedTask, setEditedTask] = useState({});
+
+    useEffect(()=>{
+        const fetchData = async () => {
+            try{
+                axios.get(`http://localhost:3000/tasks/${id}`)
+                .then(res => setPickedTask(res.data));
+            }catch(err){
+                console.log("Deu erro, meu patrão ==> ", err)
+            }
+        }
+        fetchData()
+    }, [id])
+
+    console.log(pickedTask)
+
+
 
     return (
         <div className={styles.container}>
-            <h1>TaskDetails</h1>
-            <h2>Task de ID {id}</h2>
+            <form>
+                <input type="text" value={pickedTask.title} contentEditable='true' />
+            </form>
         </div>
     );
 };
